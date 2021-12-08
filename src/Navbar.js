218a91e785f9e4a62ee76e13fae4820075c598a5
Tomy <martineztomy74@gmail.com>
2021-12-07 22:40:51 -0300

    3

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return(
+        <header>
+            <h1>E-COMMERCE</h1>
+            <nav>
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
